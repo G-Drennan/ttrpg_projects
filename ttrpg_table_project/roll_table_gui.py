fa77d63eca6086469694262844dfdc125c952aec
Ro_gdre5438_bot <64570776+G-Drennan@users.noreply.github.com>
2026-09-02 15:54:05 +1000

    Refactor roll_table_gui.py for improved readability

--- ttrpg_table_project/roll_table_gui.py
+++ ttrpg_table_project/roll_table_gui.py
@@ -17,11 +17,11 @@ class CGUI_streamlit:
        
         search_text = st.text_input(label='Search', type='search', key='1')  
         input_password = st.text_input(label='Dev Password - required for adding tables, removing tables and editing tags.', type='password', key='3') 
-        password = 'W1z@rd5'  
-        password_entered = False
-        if password == input_password:
-             password_entered = True
-        if password_entered:
+        dev_mode_password = 'W1z@rd5'  
+        self.password_entered = False
+        if dev_mode_password == input_password:
+             self.password_entered = True 
+        if self.password_entered:
             new_table_txt = st.text_input(
                 label="Create New Roll Table",
                 placeholder="Table Name; Entry 1, Entry 2, Entry 3",
@@ -46,47 +46,26 @@ class CGUI_streamlit:
                 self.tl._create_new_table(txt = new_table_txt)  #CTableLibrary
                 st.rerun() 
         
-        RollTables_filtered = self._search_bar(search_text) 
-        self._display_tables(RollTables_filtered, password_entered)      
+        self._display_tables(self.tl.search_bar(search_text))    #CRolTable  
     
 
-
-    def _search_bar(self, search_input: str):
-        items = search_input.split(' ')  
-        corrected_items = []
-        for i in items:
-            if len(i) > 0: #remove empty str
-                corrected_items.append(i) 
-
-        if len(corrected_items) > 0:
-                items = corrected_items 
-    
-        RollTables_filtered = None   
-        for i in items:  
-            RollTables_filtered = self.tl.matches_search(i, RollTables_filtered)
-
-        return RollTables_filtered  
-
-
-          
-
-    def _display_tables(self, RollTables_filtered: list, password_entered = False): #displayes given tables, enalbes control of what tables are displayed.
+    def _display_tables(self, RollTables_filtered: list): #displayes given tables - a list of #CRolTable-, enalbes control of what tables are displayed.
         for rt in RollTables_filtered:  #CRolTable
             self._GUI_fragment_roll_display_print(rt)  
 
-            self._GUI_fragment_tags(rt, password_entered)
+            self._GUI_fragment_tags(rt)
 
-            self._GUI_fragment_del(rt, RollTables_filtered, password_entered) 
+            self._GUI_fragment_del(rt, RollTables_filtered) 
 
-    def _GUI_fragment_del(self, rt: CRollTable, RollTables_filtered: list, password_entered = False):#""" 
-            if password_entered:
+    def _GUI_fragment_del(self, rt: CRollTable, RollTables_filtered: list):#""" #CRollTable 
+            if self.password_entered:
                 if st.button("Delete Table", key=rt.get_id()+'5'):
                     table_id = rt.get_id() #CRolTable
                     self.tl.del_table(table_id, RollTables_filtered) 
                     st.rerun() 
 
-    def _GUI_fragment_tags(self, rt: CRollTable, password_entered = False):
-        if password_entered: 
+    def _GUI_fragment_tags(self, rt: CRollTable):#CRollTable 
+        if self.password_entered: 
             new_tags = st.text_input(label='Enter New Tags: Delimeter is \',\' e.g Tag 1, Tag 2', type='default', key=rt.get_id()+'6') 
             if st.button("Modify Tags", key=rt.get_id()+'4'):
                 self.tl.add_tags(new_tags, rt=rt) #CRolTable
@@ -96,15 +75,15 @@ class CGUI_streamlit:
                 self.tl.remove_tags(rt=rt) 
                 st.rerun() 
     
-    def _GUI_fragment_roll_display_print(self, rt: CRollTable):  
+    def _GUI_fragment_roll_display_print(self, rt: CRollTable):  #CRollTable 
     
             st.markdown(f"## {rt.name}")
             tab = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-            st.markdown(f"*Tags: {rt._unpack_list(rt.tags)}{tab}-{tab}Dice Type: {rt.rollExpression}*") #CRolTable
+            st.markdown(f"*Tags: {rt.unpack_list(rt.tags)}{tab}-{tab}Dice Type: {rt.rollExpression}*") #CRolTable
     
             if st.button("🎲 Roll", key=rt.get_id()+'1'): 
                 
-                st.write(rt.roll_value()) #CRollTable 
+                st.write(rt.roll_value()) #CRollTable  
     
             if st.button("Display Table", key=rt.get_id()+'2'): 
                         text = rt.markdown_render(task = 'output_txt') #CRolTable 
