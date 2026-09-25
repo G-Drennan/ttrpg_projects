73e07f92a1c9c61d47728fbc90a36abc13e3ee85
Giaan <gdre5438@uni.sydney.edu.au>
2026-09-25 15:11:15 +1000

    mod for local use

--- ttrpg_table_project/roll_table_reader_base.py
+++ ttrpg_table_project/roll_table_reader_base.py
@@ -122,7 +122,7 @@ class CRollTable:   #Holds data related to the table
 #~
 
 class CTableLibrary:    #contains an list of CRollTables
-    def __init__(self, fp= Path("./tables/rollTableLibrary.json")):  
+    def __init__(self, fp= Path("/home/giaan/Desktop/ttrpg_projects/ttrpg_table_project/tables/rollTableLibrary.json")):  
         #check the path exists if not creaet the file
         if not fp.parent.exists(): #file will always exist 
             fp.parent.mkdir(parents=True, exist_ok=True)
